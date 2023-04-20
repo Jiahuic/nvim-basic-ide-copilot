ee1bc71421c5c68b1e61caf56b8355c203673047
jiahuic <jiahuic@smu.edu>
2023-04-20 10:20:31 -0400

    add tex disable wrap and back to line

--- init.lua
+++ init.lua
@@ -21,3 +21,31 @@ require "user.alpha"
 -- require "user.lsp"
 require "user.dap"
 require "user.copilot"
+
+-- Wrap the function and autocmd inside a lua block
+vim.cmd([[
+augroup DisableWrapForLatex
+  autocmd!
+  autocmd BufEnter * lua if vim.bo.filetype == 'tex' then vim.opt.wrap = true end
+augroup END
+]])
+
+-- Create an autocmd to restore cursor position when opening a file
+vim.cmd([[
+augroup RestoreCursorPosition
+  autocmd!
+  autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+augroup END
+]])
+
+-- I didn't includes the above code to options.lua because I don't know how to.
+-- Here is one attempt I made: (failed)
+-- -- Function to disable line wrapping for LaTeX files
+-- local function disable_wrap_for_latex()
+--   if vim.bo.filetype == 'tex' then
+--     vim.opt.wrap = false
+--   end
+-- end
+--
+-- -- Create an autocmd to call the function on opening a buffer
+-- vim.cmd('autocmd BufEnter * lua disable_wrap_for_latex()')
